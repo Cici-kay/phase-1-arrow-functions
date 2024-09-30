7b39077a1cd625eac647453ebdb3131f258f7689
Cynthia <kalechecynthia@gmail.com>
2024-09-30 23:36:51 +0300

    works on lab

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(number1, number2){
+    return number1 / number2
+}
+divide(2000,100)
+
+const square = number => number * number
+square(2)
+
+const add = (number1, number2) => number1 + number2
+add(3,4)
